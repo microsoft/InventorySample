6bea8c8dbd9f4137aea46c5c02902cd7df3a3d37
Alvaro Mateos <alvaroma@live.com>
2018-04-21 12:56:45 +0200

    Add AppLog data

---
+++ src/Inventory.Data/Data/AppLog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Data
+{
+    public enum LogType
+    {
+        Information,
+        Warning,
+        Error
+    }
+
+    public class AppLog
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public string Name { get; set; }
+
+        [Required]
+        public DateTimeOffset DateTime { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string User { get; set; }
+
+        [Required]
+        public LogType Type { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Source { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Action { get; set; }
+
+        [Required]
+        [MaxLength(400)]
+        public string Message { get; set; }
+
+        [MaxLength(4000)]
+        public string Description { get; set; }
+    }
+}
---
+++ src/Inventory.Data/DataContexts/AppLogDb.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Inventory.Data;
+
+namespace Inventory.Services
+{
+    public class AppLogDb : DbContext
+    {
+        private string _connectionString = null;
+
+        public AppLogDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(_connectionString);
+        }
+
+        public void EnsureCreated()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<AppLog> Logs { get; set; }
+
+        public async Task<AppLog> GetLogAsync(long id)
+        {
+            return await Logs.Where(r => r.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IList<AppLog>> GetLogsAsync(int skip, int take, DataRequest<AppLog> request)
+        {
+            IQueryable<AppLog> items = GetLogs(request);
+
+            // Execute
+            var records = await items.Skip(skip).Take(take)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return records;
+        }
+
+        public async Task<IList<AppLog>> GetLogKeysAsync(int skip, int take, DataRequest<AppLog> request)
+        {
+            IQueryable<AppLog> items = GetLogs(request);
+
+            // Execute
+            var records = await items.Skip(skip).Take(take)
+                .Select(r => new AppLog
+                {
+                    Id = r.Id,
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return records;
+        }
+
+
+        private IQueryable<AppLog> GetLogs(DataRequest<AppLog> request)
+        {
+            IQueryable<AppLog> items = Logs;
+
+            // Query
+            if (!String.IsNullOrEmpty(request.Query))
+            {
+                items = items.Where(r => r.Message.Contains(request.Query.ToLower()));
+            }
+
+            // Where
+            if (request.Where != null)
+            {
+                items = items.Where(request.Where);
+            }
+
+            // Order By
+            if (request.OrderBy != null)
+            {
+                items = items.OrderBy(request.OrderBy);
+            }
+            if (request.OrderByDesc != null)
+            {
+                items = items.OrderByDescending(request.OrderByDesc);
+            }
+
+            return items;
+        }
+
+        public async Task<int> GetLogsCountAsync(DataRequest<AppLog> request)
+        {
+            IQueryable<AppLog> items = Logs;
+
+            // Query
+            if (!String.IsNullOrEmpty(request.Query))
+            {
+                items = items.Where(r => r.Message.Contains(request.Query.ToLower()));
+            }
+
+            // Where
+            if (request.Where != null)
+            {
+                items = items.Where(request.Where);
+            }
+
+            return await items.CountAsync();
+        }
+
+        public async Task<int> CreateLogAsync(AppLog appLog)
+        {
+            appLog.DateTime = DateTime.UtcNow;
+            Entry(appLog).State = EntityState.Added;
+            return await SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteLogsAsync(params AppLog[] logs)
+        {
+            Logs.RemoveRange(logs);
+            return await SaveChangesAsync();
+        }
+
+        public async Task MarkAllAsReadAsync()
+        {
+            var items = await Logs.Where(r => !r.IsRead).ToListAsync();
+            foreach (var item in items)
+            {
+                item.IsRead = true;
+            }
+            await SaveChangesAsync();
+        }
+    }
+}
